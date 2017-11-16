9b0740529a27595d0254cdd9aa51aa65c3b2c2aa
Graham Weldon <graham@grahamweldon.com>
2017-11-16 11:48:34 +0900

    Rename cookbook to apache_spark_ng (I hate myself, I hate myself, but there are no better options)
    Signed-off-by: Graham Weldon <graham@grahamweldon.com>

--- metadata.rb
+++ metadata.rb
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-name 'apache_spark'
+name 'apache_spark_ng'
 maintainer 'ClearStory Data, Inc.'
 maintainer_email 'mbautin@clearstorydata.com'
 license 'Apache License 2.0'
